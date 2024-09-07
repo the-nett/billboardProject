ad0c2f94ba7f1b41da48e15143580caf5db702ef
SOPORTE UAA <SOPORTE UAA@DESKTOP-AN8I9UO>
2024-09-07 11:58:01 -0500

    Add project files.

---
+++ billboard/Context/BilllboardDBContext.cs
@@ -0,0 +1,14 @@
+﻿using billboard.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace billboard.Context
+{
+    public class BilllboardDBContext : DbContext
+    {
+        public BilllboardDBContext(DbContextOptions options): base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ billboard/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace billboard.Model
+{
+    public class User
+    {
+        [Key]
+        int Id { get; set; }
+    }
+}
