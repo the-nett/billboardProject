0f6b9f8ec5be5011d209df107091501798290a85
Sara Vargas <saravargas864@gmail.com>
2024-10-09 23:17:09 -0500

    Trigger auditoria Log

---
+++ billboard/Migrations/20241010005412_addAuditInsertTrigger_Document_update.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace billboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAuditInsertTrigger_Document_update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE TRIGGER trg_AuditLogOnUpdate_Document
+            ON dbo.Documents
+            AFTER UPDATE
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+
+                INSERT INTO DocumentAuditLog (DocumentId, DocumentName, Operation)
+                SELECT 
+                    i.DocumentId, 
+                    i.DocumentName, 
+                    'UPDATE'
+                FROM 
+                    inserted i;
+            END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER trg_AuditLogOnUpdate_Document");
+        }
+    }
+}
